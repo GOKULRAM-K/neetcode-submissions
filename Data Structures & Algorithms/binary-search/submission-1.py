fc56a49bfe554e4995694cac98f278feb55b455c
GOKUL RAM K <gokul.ram.kannan210905@gmail.com>
2026-05-28 19:31:28 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums)-1
+        mid = int((start+end)/2)
+        while (start <= end):
+            if target < nums[mid]:
+                end = mid-1
+                
+            elif target > nums[mid]:
+                start = mid+1
+                
+            else:
+                return mid
+            
+            mid = (start + end)//2
+        # if nums[mid]==target:
+        #     return mid
+        return -1
+        
